ab6d9f65835032d35878e513a1c08059292fef2f
rbakersmith <oztidal@hotmail.com>
2013-06-07 10:47:59 +1000

    Fixed broken commit on TeamsDemo

--- 2013.May/LINQjsDemo.Web/Scripts/Demo/TeamsDemo.js
+++ 2013.May/LINQjsDemo.Web/Scripts/Demo/TeamsDemo.js
@@ -1,1 +1,17 @@
-﻿
+﻿/// <reference path="../linq.js" />
+/// <reference path="../linq.jquery.js" />
+
+var TeamsDemo = {
+    GetTeamArray: function () {
+        var teamTableEnum = $("#teamTable tbody tr").toEnumerable();
+        var teamEnum = teamTableEnum.select(function (x) {
+            var team = x.find(".team").text();
+            var city = x.find(".city").text();
+            var state = x.find(".state").text();
+
+            return { Team: team, City: city, State: state };
+        });
+
+        return teamEnum.toArray();
+    }
+};
